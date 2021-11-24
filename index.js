528fe2aa88611694c525718af4a3ed1cc85f2439
jaebooker <jaesonbooker@gmail.com>
2021-11-24 12:31:36 +0000

    updated json response

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const port = process.env.PORT || 80
 
 app.get('/', async (req, res) => {
   console.log(req.query);
-  let stocks = await fetch(`https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=1&from=1631022248&to=1631627048&token=${process.env.API_KEY}`)
+  let stocks = await fetch(`https://finnhub.io/api/v1/quote?symbol=AAPL&token=${process.env.API_KEY}`)
   let stocksResponse = await stocks.json();
 
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
   res.send([
-    {response: stocksResponse}
+    {name: "AAPL", price: stocksResponse.c}
   ]);
 })
 
